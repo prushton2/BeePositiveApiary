cbf353d6a1d5baee32d87910c9423592a8c87e34
prushton2 <peterrushton418@gmail.com>
2022-02-02 15:47:44 -0500

    Switched DB to express, no longer needs a cors bypass

--- orders.js
+++ orders.js
@@ -1,18 +1,28 @@
 doc = document.getElementById("sec-214c")
-dburl = "https://beepositiveapiarynodeserver.prushton.repl.co/"
+dburl = "https://database.beepositiveapiary.com/"
 
 renderpage = async() => {
-    orders = JSON.parse(await getURLWithoutCors(dburl+"get/"+document.getElementById("pswdinput").value+"/orders"))
-    allHTML = ""
+    incomplete = ""
+    complete = ""
+    orders = JSON.parse(await fetch(dburl+"get/"+document.getElementById("pswdinput").value+"/orders").then(data => {return data.text()}))
     for(var order in orders) {
-        allHTML += createItemHTML(orders[order])
+        if(orders[order]["isComplete"]) {
+            complete += createItemHTML(orders[order])
+        } else {
+            incomplete += createItemHTML(orders[order])
+        }
     }
-    doc.innerHTML = `<div class="u-clearfix u-sheet u-sheet-1">${allHTML}</div>`
+    doc.innerHTML = `<div class="u-clearfix u-sheet u-sheet-1">Incomplete<br><br><br>${incomplete}<br><br><br>Complete<br><br><br>${complete}</div>`
 }
 
 createItemHTML = (order) => {
     date = new Date(parseInt(order["date"]))
-    html = `Order for <b>${order["name"]}</b> placed on <b>${date.toString()}:</b><br>`
+    if(order["isComplete"]) {
+        html = `Order for <b>${order["name"]}</b> placed on <b>${date.toString()}:</b> <button onClick="markAsIncomplete(${order["id"]})">Mark as incomplete</button><br>`
+    } else {
+        html = `Order for <b>${order["name"]}</b> placed on <b>${date.toString()}:</b> <button onClick="markAsComplete(${order["id"]})">Mark as complete</button><br>`
+    }
+    
     order["items"] = JSON.parse(order["items"])
     for(var item in order["items"]) {
         html += `${order["items"][item]}x ${item}<br>`
@@ -21,3 +31,13 @@ createItemHTML = (order) => {
 }
 
 renderpage()
+
+markAsComplete = async (i) => {
+    await fetch(`${dburl}complete/${i}/true`)
+    await renderpage()
+}
+
+markAsIncomplete = async (i) => {
+    await fetch(`${dburl}complete/${i}/false`)
+    await renderpage()
+}
