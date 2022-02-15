5dadd1f8ce137d0e8f714482d27568c61cb49ef1
prushton2 <peterrushton418@gmail.com>
2022-02-15 12:33:33 -0500

    Renders HTML and sends an edit request to the database

--- edit.js
+++ edit.js
@@ -1,25 +1,42 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-doc = document.getElementById("sec-ff05")
+doc = document.getElementById("sec-9fb3")
 
-orderID = null
-password = null
-
-function getOrder(id, password) {
-    orderID = urlParams.get("ID")
-    password = urlParams.get("pswd")
-}
+shoppingList = {}
+orderID = urlParams.get("ID")
+password = urlParams.get("pswd")
 
 renderpage = async(orderID, password) => {
     order = await fetch(`${dburl}getorder/${orderID}/${password}`).then(value => {return value.text()})
-    console.log(order)
-    doc.innerHTML = `<div class="u-clearfix u-sheet u-sheet-1">Incomplete<br><br><br>${incomplete}<br><br><br>Complete<br><br><br>${complete}</div>`
+    order = JSON.parse(order)["response"]
+    order["items"] = JSON.parse(order["items"])
+    shoppingList = order["items"]
+
+    html = ""
+    document.getElementById("totalCost").innerHTML = `${getDisplayCost(shoppingList)}`
+
+    for(let item in order["items"]) {
+        console.log(item)
+        html += createItemHTML(products[item], order["items"][item])
+    }
+    doc.innerHTML = `   <div class="u-clearfix u-sheet u-sheet-1"><b>Your Order:</b><br><br>${html}</div>`
 }
 
 function createItemHTML(item, count) {
     return `<div><div><label>${item["name"]}</label><br><label>${item["description"]}</label></div><div style='text-align: right;'><label>$${item["price"]}</label>&nbsp&nbsp&nbspx<input onchange="updateShoppingList()" id="Count of ${item["name"]}" value=${count}></div></div><br>`
 }
 
-getOrder()
+function updateShoppingList() {
+    for(var key in shoppingList) {
+        shoppingList[key] = parseInt(document.getElementById(`Count of ${key}`).value)
+    }
+    document.getElementById("totalCost").innerHTML = `${getDisplayCost(shoppingList)}`
+}
+
+saveEdits = async() => {
+    response = await fetch(`${dburl}setorder/${orderID}/${password}/${JSON.stringify(shoppingList)}`).then(value => {return value.text()})
+    console.log(response)
+}
+
 renderpage(orderID, password)
