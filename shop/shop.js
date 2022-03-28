281f47de74474755c8f263a815ec77cf24e63b60
prushton2 <peterrushton418@gmail.com>
2022-03-28 11:26:46 -0400

    addToCart now works with the itemInfo class

--- shop/shop.js
+++ shop/shop.js
@@ -15,13 +15,11 @@ shoppingList = JSON.parse(shoppingList)
 
 async function addToCart(item, hasSubproduct=false) { //Subproduct is used for something like a jar of honey, where the jar is the subproduct and the honey is the item
     amountToAdd = parseFloat(document.getElementById(`Count of ${item}`).value)
-    console.log(amountToAdd)
-    amountToAdd = await setItemAmountToIncrement(item, amountToAdd)
-    console.log(amountToAdd)
+    amountToAdd = await products.setItemAmountToIncrement(item, amountToAdd)
     
     if(hasSubproduct) { //if we have a subproduct, we need to add the subproduct to the shoppinglist and make sure the amount of the subproduct follows the increment of the item
         subProduct = document.getElementById(`Subproduct of ${item}`).value
-        amountToAdd = await setItemAmountToIncrement(subProduct, amountToAdd)
+        amountToAdd = await products.setItemAmountToIncrement(subProduct, amountToAdd)
     } else {
         subProduct = 0 //0 is the empty value for subproduct
     }
@@ -29,7 +27,7 @@ async function addToCart(item, hasSubproduct=false) { //Subproduct is used for s
     for(i in shoppingList["Items"]) { //Look for the item if it is already in the shoppinglist.
         if(shoppingList["Items"][i]["productID"] == item && shoppingList["Items"][i]["subProductID"] == subProduct) {
             shoppingList["Items"][i]["amount"] += amountToAdd //Add the amount to the existing item in the shoppingList and return
-            // checkout()
+            checkout()
             return;
         }
     }
@@ -48,10 +46,10 @@ async function addToCart(item, hasSubproduct=false) { //Subproduct is used for s
         }
         return
     }
-    // checkout()
+    checkout()
 }
 
 function checkout() { //save the shoppinglist and go to the checkout
     localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
-    window.location.href = "../checkout/Checkout.html"
+    // window.location.href = "../checkout/Checkout.html"
 }
