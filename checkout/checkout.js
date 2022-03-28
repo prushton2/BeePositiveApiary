ee5074a633c3ab66ba2878834c01579075082b14
prushton2 <peterrushton418@gmail.com>
2022-03-28 15:33:27 -0400

    Fixed bug where 2 items with different subproducts were treated as one product

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -4,7 +4,7 @@ function createHTML(item) { //shoppingList[item], supposed to contain amount and
     let subProductLabel = item["subProductID"] == 0 ? "" : `${products.getProduct(item["subProductID"])["name"]} of`
     let label = `<label>${subProductLabel} ${products.getProduct(item["productID"])["name"]}</label>`
     let price = `<label>$${products.getProduct(item["productID"])["price"]}</label>&nbsp&nbsp&nbspx`
-    let textbox = `<input style="width: 75px;" type="number" step="any" onchange="updateShoppingList()" id="Count of ${item["productID"]}" value=${item["amount"]}>`
+    let textbox = `<input style="width: 75px;" type="number" step="any" onchange="updateShoppingList()" id="Count of ${item["productID"]} ${item["subProductID"]}" value=${item["amount"]}>`
 
     return `<div>${label}<div style='float:right; text-align: right;'>${price}${textbox}</div></div><br>`
 }
@@ -12,7 +12,7 @@ function createHTML(item) { //shoppingList[item], supposed to contain amount and
 async function drawCheckout() {
     await products.getProducts() 
     document.getElementById("CheckoutList").innerHTML = `<div class="u-clearfix u-sheet u-sheet-1">${createAllHtml(shoppingList)}</div>`
-    // document.getElementById("totalCost").innerHTML = `${getTaxCalculation(shoppingList["Items"])}<br>Total Cost: ${getDisplayCost(shoppingList["Items"])}`
+    document.getElementById("totalCost").innerHTML = `${getTaxCalculation(shoppingList["Items"])}<br>Total Cost: ${getDisplayCost(shoppingList["Items"])}`
 }
 
 function createAllHtml(jsonobject) { //takes in the shoppinglist
@@ -37,7 +37,7 @@ async function updateShoppingList() {
     for(key in shoppingList["Items"]) {
         item = shoppingList["Items"][key]
 
-        newAmount = parseFloat(document.getElementById(`Count of ${item["productID"]}`).value)
+        newAmount = parseFloat(document.getElementById(`Count of ${item["productID"]} ${item["subProductID"]}`).value)
         
         
         //set increment to subproductID if it isnt 0 else set it to productID
@@ -51,7 +51,7 @@ async function updateShoppingList() {
         if(newAmount <= 0) {
             shoppingList["Items"].splice(key, 1)
         } else {
-            document.getElementById(`Count of ${item["productID"]}`).value = newAmount
+            document.getElementById(`Count of ${item["productID"]} ${item["subProductID"]}`).value = newAmount
             shoppingList["Items"][key]["amount"] = newAmount
         }
     }
