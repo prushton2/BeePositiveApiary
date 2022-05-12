c43e61becc9e6c1f1e570267379df91c9d57dd8b
prushton2 <peterrushton418@gmail.com>
2022-05-12 16:40:03 -0400

    Checkout fully works now

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -3,7 +3,7 @@ import * as utils from "../utils.js"
 let shoppingList = JSON.parse(localStorage.getItem("shoppingList"))
 
 export async function createHTML(item) { //shoppingList[item], supposed to contain amount and subproductID
-    let textbox = `<input style="width: 75px;" type="number" step="any" onchange="updateShoppingList()" id="Count of ${item["productID"]} ${item["subProductID"]}" value=${item["amount"]}>`
+    let textbox = `<input style="width: 75px;" type="number" step="any" name="checkoutItemCount" id="Count of ${item["productID"]} ${item["subProductID"]}" value=${item["amount"]}>`
     
     let htmlString = `<div> <label>{productName}</label> <div style='float:right; text-align: right;'>{price}&nbsp&nbsp&nbspx${textbox}</div></div><br>`
 
@@ -14,15 +14,22 @@ export async function drawCheckout() {
     await utils.products.getProducts() 
     document.getElementById("CheckoutList").innerHTML = await createAllHtml(shoppingList)//`<div class="u-clearfix u-sheet u-sheet-1">${createAllHtml(shoppingList)}</div>`
     document.getElementById("totalCost").innerHTML = `${await utils.products.getTaxCalculation(shoppingList["Items"])}<br>Total Cost: ${await utils.products.getDisplayCost(shoppingList["Items"])}`
+
+    for(let i = 0; i < document.getElementsByName("checkoutItemCount").length; i++) {
+        document.getElementsByName("checkoutItemCount")[i].addEventListener("change", () => {
+            updateShoppingList()
+        })
+    }
+
+
 }
 
 export async function createAllHtml(jsonobject) { //takes in the shoppinglist
     let html = ""
     for(let key in jsonobject["Items"]) {
-        let productID = jsonobject["Items"][key]["productID"]        
         html += await createHTML(jsonobject["Items"][key])
     }
-
+    
     return html == "" ? "Your items will appear here when they have been added to the cart!" : html
 }
 
@@ -34,18 +41,9 @@ export function gotofinalize() {
 export async function updateShoppingList() {
     
     for(let key in shoppingList["Items"]) {
-        item = shoppingList["Items"][key]
+        let item = shoppingList["Items"][key]
 
-        newAmount = parseFloat(document.getElementById(`Count of ${item["productID"]} ${item["subProductID"]}`).value)
-        
-        
-        //set increment to subproductID if it isnt 0 else set it to productID
-        if(item["subProductID"] != 0) {
-            newAmount = await products.setItemAmountToIncrement(item["subProductID"], newAmount)
-        } else {
-            newAmount = await products.setItemAmountToIncrement(item["productID"], newAmount)
-        }
-
+        let newAmount = parseFloat(document.getElementById(`Count of ${item["productID"]} ${item["subProductID"]}`).value)
         
         if(newAmount <= 0) {
             shoppingList["Items"].splice(key, 1)
@@ -70,4 +68,4 @@ export function resetShoppingCart() {
     }
 }
 
-drawCheckout()
+await drawCheckout()
