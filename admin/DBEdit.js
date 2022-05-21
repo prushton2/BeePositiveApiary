a34a3b6bd1519d88d54d6d46e31f9a2949afe86e
prushton2 <peterrushton418@gmail.com>
2022-05-21 16:11:48 -0400

    Added deletion and addition, removed queue, added editing of primary keys

--- admin/DBEdit.js
+++ admin/DBEdit.js
@@ -3,28 +3,31 @@ import * as config from "../config.js"
 
 export let loadedDBName = ""
 export let loadedDB
-export let queuedEdits = {"edits": []}
 
 let password = document.getElementById("pswdinput")
 let uneditableColumns = ["id", "subProductID"]
 
 window.loadDB = loadDB
-window.saveDB = saveDB
-window.queueEdit = queueEdit
-window.getEdits = getEdits
+window.saveEdit = saveEdit
+window.queueEdit = saveEdit
+window.deleteEntry = deleteEntry
+window.newEntry = newEntry
+
+password.value = "password"
 
 export async function loadDB(name) {
+
     let primaryKeyColumns
 
     await utils.products.getProducts(true)
 
     switch(name) {
-        case "products":
+        case "Products":
             loadedDBName = "Products"
             loadedDB = utils.products.products
             primaryKeyColumns = ["id"]
             break
-        case "productRelations":
+        case "ProductRelations":
             loadedDBName = "ProductRelations"
             loadedDB = utils.products.productRelations
             primaryKeyColumns = ["id", "subProductID"]
@@ -36,39 +39,11 @@ export async function loadDB(name) {
 
 
 
-    document.getElementById("databaseEditor").innerHTML = loadEditableDB(loadedDB, primaryKeyColumns)
-    queuedEdits = {"edits": []}
+    document.getElementById("databaseEditor").innerHTML = await loadEditableDB(loadedDB, primaryKeyColumns)
 }
 
-export async function saveDB() {
-    for(let edit in queuedEdits["edits"]) {
-        let index = edit
-        edit = queuedEdits["edits"][edit]
-        let column = edit["column"]
-        let primaryKeys = edit["primaryKeys"]
-        let value = edit["value"]
-        let table = loadedDBName
-        let response = await utils.httpRequest(`${config.dburl}/db/update`, "PATCH", 
-        {
-            "password": password.value,
-            "table": table,
-            "primaryKeys": primaryKeys,
-            "column": column,
-            "value": value
-        })
-        if(response["response"] != "Product Updated") {
-            alert(`There was an error updating the ${column} column on the ${table} table under the keys ${primaryKeys} to value ${value}:\n ${response["response"]}`)
-        } else {
-            queuedEdits["edits"][index] = null
-        }
-    }
-    queuedEdits["edits"] = queuedEdits["edits"].filter(value => value != null)
-    //get the current time in HH:MM
-    let time = new Date().toLocaleTimeString()
-    document.getElementById("response").innerHTML = `${JSON.stringify(queuedEdits) == '{"edits":[]}' ? "All values updated successfully" : "Some values were not updated"} (${time})`
-}
-
-export function loadEditableDB(db, primaryKeyColumns) {
+export async function loadEditableDB(db, primaryKeyColumns) {
+    await utils.products.getProducts(true)
     let html = "<style> table { border-collapse:collapse; border:1px solid #000000; }\n table td {border:1px solid #000000;} </style>"
     html += `<table>`
     //create column headers
@@ -76,28 +51,65 @@ export function loadEditableDB(db, primaryKeyColumns) {
     for(let column in columns) {
         html += `<th>${column}</th>`
     }
+    html += `<th>Action&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</th>`
 
     for(let entry in db) {
         html += `<tr>`
+
+        let primaryKeys = {}
+        primaryKeyColumns.forEach(key => primaryKeys[key] = db[entry][key]) //create a json object of the primary keys
+
         for(let column in db[entry]) {
-            if(uneditableColumns.includes(column)) {
-                html += `<td> <label>${db[entry][column]}</label> </td>`
-            } else {
-                let primaryKeys = {}
-                primaryKeyColumns.forEach(key => primaryKeys[key] = db[entry][key])
-                let functionString = `queueEdit( ${ JSON.stringify(primaryKeys) } , \"${column}\", \"${entry}\")`
-                html += `<td> <input onchange='${functionString}' id="Entry for ${entry} ${column}" type="${typeof db[entry][column]}" value="${db[entry][column]}"> </td>`
-            }
+            let functionString = `saveEdit( ${ JSON.stringify(primaryKeys) } , \"${column}\", \"${entry}\")` //create a function to be called when the value is changed
+            html += `<td> <input onchange='${functionString}' id="Entry for ${entry} ${column}" type="${typeof db[entry][column]}" value="${db[entry][column]}"> </td>` //create an input field with the value of the column
         }
+        html += `<td> <button style="width: 100%;" onclick='deleteEntry(${JSON.stringify(primaryKeys)})'>Delete</button> </td>` //create a button to delete the entry
         html += `</tr>`
     }
+
+    html += "<tr>"
+
+    for(let index in Object.keys(db[0])) {
+        let column = Object.keys(db[0])[index]
+        html += `<td> <input id="New Entry #${index}" type="${typeof db[0][column]}" name="${column}"> </td>`
+    }
+
+    html += `<td> <button style="width: 100%;" onclick='newEntry(${Object.keys(db[0]).length})'>Add Entry</button> </td></tr>`
 
     html += `</table>`
 
     return html
 }
 
-export function queueEdit(primaryKeys, column, entryID) {
+export async function newEntry(length) {
+    let table = {}
+    for(let index = 0; index < length; index++) {
+        let element = document.getElementById("New Entry #" + index)
+        let column = element.getAttribute("name")
+        let value = element.value
+        
+        switch(element.type) {
+            case "number":
+                value = parseFloat(value)
+                break
+            case "text":
+                value = value.toString()
+                break
+        }
+        
+        table[column] = value
+    }
+
+    let response = await utils.httpRequest(`${config.dburl}/db/newEntry`, "POST",
+    {
+        "password": password.value,
+        "table": loadedDBName,
+        "values": table
+    }, false)
+    await loadDB(loadedDBName)
+}
+
+export async function saveEdit(primaryKeys, column, entryID) {
 
     let element = document.getElementById(`Entry for ${entryID} ${column}`) //get the element that was changed
     let value = element.value //get the new value
@@ -111,25 +123,30 @@ export function queueEdit(primaryKeys, column, entryID) {
             break
     }
 
-    let editExists = false //check if the edit already exists (if it does, overwrite it)
-    for(let edit in queuedEdits["edits"]) {
-        edit = queuedEdits["edits"][edit]
-        
-        if(JSON.stringify(edit["primaryKeys"]) == JSON.stringify(primaryKeys) && edit["column"] == column) {
-            edit["value"] = value
-            editExists = true
-        }
-    }
-
-    if(!editExists) { //if the edit doesn't exist, add it
-        queuedEdits["edits"].push({
-            "primaryKeys": primaryKeys,
-            "column": column,
-            "value": value
-        })
-    }
+    let response = await utils.httpRequest(`${config.dburl}/db/update`, "PATCH",
+    {
+        "password": password.value,
+        "table": loadedDBName,
+        "primaryKeys": primaryKeys,
+        "column": column,
+        "value": value  
+    }, true)
 }
 
-export function getEdits() {
-    return queuedEdits
+
+export async function deleteEntry(primaryKeys) {
+    if(confirm(`Are you sure you want to delete this entry?\n${JSON.stringify(primaryKeys)}`)) {
+        let response = await utils.httpRequest(`${config.dburl}/db/deleteEntry`, "DELETE",
+        {
+            "password": password.value,
+            "table": loadedDBName,
+            "primaryKeys": primaryKeys
+        })
+        if(response["response"] != "Entry Deleted") {
+            alert(`There was an error deleting the entry under the keys ${JSON.stringify(primaryKeys)}:\n ${response["response"]}`)
+        } else {
+            alert("Entry deleted")
+        }
+    }
+    await loadDB(loadedDBName)
 }
